6422e5a8b5532ecbf9f179ff4a7146249b53e886
M-ute <wiaboamos76@gmail.com>
2026-03-09 21:45:01 -0800

    components created

--- food-app/app/layout.tsx
+++ food-app/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/src/components/Navbar";
+import Notification from "@/src/components/Notification";
+import { Footer } from "@/src/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar/>
+        <Notification/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
